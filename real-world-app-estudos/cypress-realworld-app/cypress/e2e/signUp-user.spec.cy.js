7a757fda0fd91ea513020ff8b1670fb2c78ea3ed
deboralili <53949599+deboralili@users.noreply.github.com>
2025-10-08 13:59:14 -0300

    Criado objetos para dados do usuario + seletores

--- real-world-app-estudos/cypress-realworld-app/cypress/e2e/signUp-user.spec.cy.js
+++ real-world-app-estudos/cypress-realworld-app/cypress/e2e/signUp-user.spec.cy.js
@@ -1,13 +1,32 @@
 describe('Real World App - Tests', () => {
+  
+  const selectorsList = {
+    signUpLink: "[data-test='signup']",
+    firstName: "[data-test='signup-first-name']",
+    lastName: "[data-test='signup-last-name']",
+    username: "[data-test='signup-username']",
+    password: "[data-test='signup-password']",
+    confirmPassword: "[data-test='signup-confirmPassword']",
+    signUpButton: "[data-test='signup-submit']"
+  }
+
+  const signUpData = {
+    firstName: 'Jessica',
+    lastName: 'Canto',
+    username: 'jess001',
+    password: 'Senh@001',
+    confirmPassword: 'Senh@001'
+  }
+
   it('SignUp User - Success', () => {
     cy.visit('http://localhost:3000/signin')
-    cy.get("[data-test='signup']").click()
-    cy.get("[data-test='signup-first-name']").type('Jessica')
-    cy.get("[data-test='signup-last-name']").type('Canto')
-    cy.get("[data-test='signup-username']").type('jess001')
-    cy.get("[data-test='signup-password']").type('Senh@001')
-    cy.get("[data-test='signup-confirmPassword']").type('Senh@001')
-    cy.get("[data-test='signup-submit']").click()
+    cy.get(selectorsList.signUpLink).click()
+    cy.get(selectorsList.firstName).type(signUpData.firstName)
+    cy.get(selectorsList.lastName).type(signUpData.lastName)
+    cy.get(selectorsList.username).type(signUpData.username)
+    cy.get(selectorsList.password).type(signUpData.password)
+    cy.get(selectorsList.confirmPassword).type(signUpData.confirmPassword)
+    cy.get(selectorsList.signUpButton).click()
     cy.location('pathname').should('equal', '/signin')
   })
 })
