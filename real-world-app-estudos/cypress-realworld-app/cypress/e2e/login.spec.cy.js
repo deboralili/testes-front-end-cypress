1660c7c66c5acf95339512ce572f2609f6b41720
deboralili <53949599+deboralili@users.noreply.github.com>
2026-01-27 14:28:25 -0300

    Implementacao dos passos do caso de teste TC-001 Login com credenciais validas

---
+++ real-world-app-estudos/cypress-realworld-app/cypress/e2e/login.spec.cy.js
@@ -0,0 +1,20 @@
+//Suite de testes (cenario de teste)
+describe('SC-001: Login com sucesso', () => {
+
+    //caso de teste
+    it('TC-001: Deve fazer login com credenciais validas', () => {
+
+        //Acessa a pagina de login
+        cy.visit("http://localhost:3000/signin");
+
+        //Preenche o campo username com nome de usuario valido
+        cy.get('[data-test="signin-username"]').type('qa_teste');
+
+        //Preenche o campo password com senha correspondente valida
+        cy.get('[data-test="signin-password"]').type('1234');
+
+        //Clica no botão para fazer login
+        cy.get('[data-test="signin-submit"]').click();
+    });
+});
+
