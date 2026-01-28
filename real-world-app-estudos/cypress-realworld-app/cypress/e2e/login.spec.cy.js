31097931da88ad6a78f05262ae7276876b7df90b
deboralili <53949599+deboralili@users.noreply.github.com>
2026-01-28 13:46:24 -0300

    Uso do intercept + Retirada do localStorage
    O intercept foi utilizado para garantir que a solicitacao de login foi bem sucedida e garante que o teste não tente validar a UI antes da resposta chegar. Retirada de LocalSorage foi feita para evitar redundancia nos testes. Ele faz mais sentido em testes de persistencia.

--- real-world-app-estudos/cypress-realworld-app/cypress/e2e/login.spec.cy.js
+++ real-world-app-estudos/cypress-realworld-app/cypress/e2e/login.spec.cy.js
@@ -3,6 +3,8 @@ describe('SC-001: Login com sucesso', () => {
 
     //caso de teste
     it('TC-001: Deve fazer login com credenciais validas', () => {
+        //Acompanha a requisicao para fazer o login
+        cy.intercept('POST', '**/login').as('loginRequest');
 
         //Acessa a pagina de login
         cy.visit("http://localhost:3000/signin");
@@ -16,24 +18,17 @@ describe('SC-001: Login com sucesso', () => {
         //Clica no botão para fazer login
         cy.get('[data-test="signin-submit"]').click();
 
+        //Espera a resposta do servidor
+        cy.wait('@loginRequest').then((interception) => {
+            //Garante que a requisicao ocorreu com sucesso
+            expect(interception.response.statusCode).to.equal(200);
+        });
+
         //Verifica se o nome de usuario esta na pagina
         cy.get('[data-test="sidenav-username"]').should('be.visible').and('contain', 'qa_teste');
 
         //Verifica se tem a guia de transacao da tela Home
         cy.get('[data-test="nav-transaction-tabs"]').should('be.visible');
-
-        //Acessa a janela do navegador
-        cy.window().then((win) => {
-
-            //Acessa localStorage
-            const data = win.localStorage.getItem('authState');
-
-            //Transforma em objeto
-            const authState = JSON.parse(data);
-
-            //Verifica se tem o valor esperado
-            expect(authState.value).to.equal('authorized');
-        });
     });
 });
 
