1b7e3ea56605b0f13707da3f21cce4306569bb25
deboralili <53949599+deboralili@users.noreply.github.com>
2026-02-05 13:11:26 -0300

    Instalando chance.js
    Foi necessario atualizar a versao do node e tirar o vite-plugin-istanbul devido a versao incompativel

--- real-world-app-estudos/cypress-realworld-app/vite.config.ts
+++ real-world-app-estudos/cypress-realworld-app/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
-import istanbul from "vite-plugin-istanbul";
+//import istanbul from "vite-plugin-istanbul";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "VITE");
@@ -20,12 +20,12 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react(),
       eslint(),
-      istanbul({
-        cypress: true,
-        requireEnv: true,
-        exclude: ["node_modules", "cypress", "dist"],
-        forceBuildInstrument: true,
-      }),
+      // istanbul({
+      //   cypress: true,
+      //   requireEnv: true,
+      //   exclude: ["node_modules", "cypress", "dist"],
+      //   forceBuildInstrument: true,
+      // }),
     ],
     // to get aws amplify to work with vite
     resolve: {
