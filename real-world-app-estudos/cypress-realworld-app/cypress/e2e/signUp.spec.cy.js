535d0da34569dcf661160c707f735444e0dd0dd7
deboralili <53949599+deboralili@users.noreply.github.com>
2026-02-07 12:54:44 -0300

    Implementado usuario randomico para cadastro

--- real-world-app-estudos/cypress-realworld-app/cypress/e2e/signUp.spec.cy.js
+++ real-world-app-estudos/cypress-realworld-app/cypress/e2e/signUp.spec.cy.js
@@ -2,11 +2,21 @@ import HomePage from "../pages/homePage";
 import LoginPage from "../pages/loginPage";
 import SignUpPage from "../pages/signUpPage";
 
+const Chance = require('chance');
+
+const chance = new Chance();
 const loginPage = new LoginPage();
 const signUpPage = new SignUpPage();
 const homePage = new HomePage();
 
 describe('Cadastro de Usuario', () => {
+
+  const randomUser = {
+    firstName: chance.first(),
+    lastName: chance.last(),
+    username: chance.animal(),
+    password: chance.timestamp()
+  }
 
   beforeEach(() => {
     signUpPage.accessSignUpPage();
@@ -17,15 +27,20 @@ describe('Cadastro de Usuario', () => {
     //TC-003: Efetuar cadastro de um novo usuário com informações válidas.
     it('Deve efetuar o cadastro com sucesso', () => {
 
-      signUpPage.signUpWithAnyUser("First", "Last", "User", "Pass");
+      signUpPage.signUpWithAnyUser(
+        randomUser.firstName,
+        randomUser.lastName,
+        randomUser.username,
+        randomUser.password
+      );
 
       loginPage.checkLoginPage();
 
-      loginPage.loginWithUser("User", "Pass");
+      loginPage.loginWithUser(randomUser.username, randomUser.password);
 
       homePage.checkHomePage();
 
-      homePage.checkUsernameLogged("User");
+      homePage.checkUsernameLogged(randomUser.username);
     });
   });
 
