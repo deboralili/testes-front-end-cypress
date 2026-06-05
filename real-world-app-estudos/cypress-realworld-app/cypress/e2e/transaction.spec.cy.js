7d0f9fda7d2384b663306ae71650bc63e9f9a550
deboralili <53949599+deboralili@users.noreply.github.com>
2026-06-05 15:33:50 -0300

    add tc-011

---
+++ real-world-app-estudos/cypress-realworld-app/cypress/e2e/transaction.spec.cy.js
@@ -0,0 +1,68 @@
+import LoginPage from "../pages/loginPage";
+import userData from "../fixtures/userData.json";
+import HomePage from "../pages/homePage";
+
+const loginPage = new LoginPage();
+const homePage = new HomePage();
+
+describe('Enviar Dinheiro', () => {
+
+    let userBalance;
+
+    beforeEach(() => {
+        //Acompanha a requisicao para fazer o login
+        cy.intercept('POST', '**/login').as('loginRequest');
+        cy.intercept('POST', '**/transactions').as('transactionRequest');
+
+        loginPage.accessLoginPage();
+
+        loginPage.fillLoginForm(userData.userNative);
+
+        loginPage.checkSignInButtonEnabled();
+
+        loginPage.clickLoginButton();
+
+        cy.wait('@loginRequest').then((interception) => {
+            //Garante que a requisicao ocorreu com sucesso
+            expect(interception.response.statusCode).to.equal(200);
+        });
+
+        homePage.getUserBalance().then((balance) => {
+            userBalance = balance;
+        });
+    });
+
+    context('Enviar dinheiro com saldo suficiente', () => {
+
+        it('Deve enviar dinheiro com sucesso', () => {
+            homePage.clickNewTransactionButton();
+            homePage.selectContact();
+
+            cy.log(userBalance);
+
+            let description = 'Saldo suficiente';
+
+            homePage.fillTransactionForm((userBalance-1), description);
+            homePage.clickPaymentButton();
+
+            //cy.pause();
+
+            cy.wait('@transactionRequest').then((interception) => {
+                expect(interception.response.statusCode).to.equal(200);
+            });
+
+            homePage.checkSuccessTransactionAlert('Transaction Submitted');
+            //homePage.checkTransactionSummary('Paid $' + (userBalance-1) + ' for ' + description);
+            //homePage.checkUpdatedUserBalance(1);
+            //homePage.checkReturnButtonEnabled();
+            //homePage.checkAnotherTransactionButtonEnabled();
+        });
+    });
+
+    /* context('Enviar dinheiro com saldo insuficiente', () => {
+
+        it('Deve exibir mensagem de erro ao enviar dinheiro sem saldo suficiente', () => {
+            
+        });
+    }); */
+}); 
--- real-world-app-estudos/cypress-realworld-app/cypress/pages/homePage.js
+++ real-world-app-estudos/cypress-realworld-app/cypress/pages/homePage.js
@@ -1,8 +1,21 @@
 class HomePage {
     selectorsList() {
         const selectors = {
+            //User Info
             usernameLogged: "[data-test='sidenav-username']",
-            transactionTab: "[data-test='nav-transaction-tabs']"
+            userBalance: "[data-test='sidenav-user-balance']",
+
+            //Transaction
+            transactionTab: "[data-test='nav-transaction-tabs']",
+            newTransactionButton: "[data-test='nav-top-new-transaction']",
+            firstContact: "[data-test='user-list-item-uBmeaz5pX']",
+            amountInput: "[data-test='transaction-create-amount-input']",
+            descriptionInput: "[data-test='transaction-create-description-input']",
+            paymentButton: "[data-test='transaction-create-submit-payment']",
+            requestButton: "[data-test='transaction-create-submit-request']",
+            returnButton: "[data-test='new-transaction-return-to-transactions']",
+            anotherTransactionButton: "[data-test='new-transaction-create-another-transaction']",
+            // successTransactionAlert: "[data-test='alert-bar-success']"
         }
 
         return selectors;
@@ -12,9 +25,56 @@ class HomePage {
         cy.get(this.selectorsList().usernameLogged).should('be.visible').and('contain', username);
     }
 
+    getUserBalance() {
+        return cy.get(this.selectorsList().userBalance).invoke('text').then((balanceText) => {
+            const balance = parseFloat(balanceText.replace(/[^0-9.-]+/g, ''));
+            return balance;
+        });
+    }
+
     checkHomePage() {
         cy.get(this.selectorsList().transactionTab).should('be.visible');
     }
+
+    clickNewTransactionButton() {
+        cy.get(this.selectorsList().newTransactionButton).click();
+    }
+
+    selectContact() {
+        cy.get(this.selectorsList().firstContact).click();
+    }
+
+    fillTransactionForm(amount, description) {
+        cy.get(this.selectorsList().amountInput).type(amount);
+        cy.get(this.selectorsList().descriptionInput).type(description);
+    }
+
+    clickPaymentButton() {
+        cy.get(this.selectorsList().paymentButton).click();
+    }
+
+    checkSuccessTransactionAlert(expectedMessage) {
+        //cy.get(this.selectorsList().successTransactionAlert).should('be.visible').and('contain', expectedMessage);
+        cy.contains(expectedMessage).should('be.visible');
+    }
+
+    checkTransactionSummary(expectedSummary) {
+        cy.contains(expectedSummary).should('be.visible');
+    }
+
+    checkUpdatedUserBalance(newBalance) {
+        cy.get(this.selectorsList().userBalance).should('be.visible').and('have.text', '$' + newBalance);
+
+    }
+
+    checkReturnButtonEnabled() {
+        cy.get(this.selectorsList().returnButton).should('be.enabled');
+    }
+
+    checkAnotherTransactionButtonEnabled() {
+        cy.get(this.selectorsList().anotherTransactionButton).should('be.enabled');
+    }
+
 }
 
 export default HomePage
